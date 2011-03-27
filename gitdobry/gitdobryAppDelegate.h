e65f7597f7cfb20d66855f85a6136e385f96f135
Jaroslaw Jacek <giku.itoa@gmail.com>
2011-03-27 12:48:16 +0200

    first

---
+++ gitdobry/gitdobryAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  gitdobryAppDelegate.h
+//  gitdobry
+//
+//  Created by Jaroslaw Jacek on 11-03-27.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gitdobryAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
